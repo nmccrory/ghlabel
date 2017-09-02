aed96fe639651a2ba7e7315a5b407044ae8d3e37
Nick McCrory <nickmhc14@gmail.com>
2017-09-01 19:17:14 -0500

    Fix issues pointed out by go report card (#7)
    * fixed issues pointed out by go report card
    
    * Add gometalinter to circle tests.
    
    * Fix syntactical errors pointed out by gometalinter
    
    * Updated build-tools to 1.5.0
    
    https://github.com/drud/build-tools/releases/tag/1.5.0
    
    * Whitespace commit
    
    * Actually consume our Execute() function
    
    * Remove the Execute() function in root.go as it is unnecessary. The main function in main.go provides the same functionality.
    
    * Manually revert to build-tools v1.4.1
    
    * remove build-tools/{.DS_Store, .appveyor.yml}

--- cmd/ghlabel/root.go
+++ cmd/ghlabel/root.go
@@ -30,16 +30,28 @@ var RootCmd = &cobra.Command{
 		client := NewClient()
 		if User != "" && Repository != "" {
 			// If we have both a user and repository then we run the tool on just that repository.
-			client.ListByUserRepository()
+			err := client.ListByUserRepository()
+			if err != nil {
+				log.Fatalf("%v", err)
+			}
 		} else if User != "" {
 			// If we Just have a user but no repository, then run the tool for all repositories for that user.
-			client.ListByUser()
+			err := client.ListByUser()
+			if err != nil {
+				log.Fatalf("%v", err)
+			}
 		} else if Organization != "" && Repository != "" {
 			// If we have an org and a repository, run the tool for just that repository.
-			client.ListByOrgRepository()
+			err := client.ListByOrgRepository()
+			if err != nil {
+				log.Fatalf("%v", err)
+			}
 		} else if Organization != "" {
 			// If we just have an org and no repository, run the tool on all repos in that org.
-			client.ListByOrg()
+			err := client.ListByOrg()
+			if err != nil {
+				log.Fatalf("%v", err)
+			}
 		} else {
 			log.Fatal("You must specify either an organization or user. Use -h for help.")
 		}
@@ -52,9 +64,4 @@ func init() {
 	RootCmd.PersistentFlags().StringVarP(&Repository, "repo", "", "", "A specific repository to sync.")
 	RootCmd.PersistentFlags().StringVarP(&Reference, "ref", "", "", "Required: the repository from which to replicate labels.")
 	RootCmd.PersistentFlags().BoolVarP(&ApplyLabels, "apply", "a", false, "Apply currently staged label changes.")
-}
-
-// Execute runs Cobra
-func Execute() {
-	RootCmd.Execute()
 }
